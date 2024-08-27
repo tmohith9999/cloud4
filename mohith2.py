e1bf83a6b1ee2abad1aae93ba10dc81058c698da
tmohith9999 <131354893+tmohith9999@users.noreply.github.com>
2024-08-27 14:02:24 +0530

    Update mohith2.py

--- mohith2.py
+++ mohith2.py
@@ -5,8 +5,8 @@ from sklearn import model_selection
 import numpy as np
 import sklearn
 from sklearn import linear_model
-X=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0]]
-y=[ 8, 10 , 12, 14, 16, 18, 20]
+X=[[4.0],[5.0],[6.0],[7.0],[8.0],[9.0],[10.0] [11,0]]
+y=[ 8, 10 , 12, 14, 16, 18, 20, 22]
 X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3, random_state=7)
 print("Training Features", X_train);print("Training Labels",y_train);print("Training Data",X_test);print("Testing Data",y_test)
 reg=linear_model.LinearRegression()
